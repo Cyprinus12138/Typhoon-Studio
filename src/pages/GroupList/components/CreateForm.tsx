15819a41471df2fed869d6721b51ee85c6ebb551
cyprinus <cyprinus12138@gmail.com>
2021-02-04 16:42:39 +0800

    Update
    Implement of basic information of CreateForm.tsx

--- src/pages/GroupList/components/CreateForm.tsx
+++ src/pages/GroupList/components/CreateForm.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProForm, {
   ModalForm,
-  ProFormText,
-  ProFormDateRangePicker,
-  ProFormSelect,
+  ProFormText, ProFormTextArea,
 } from '@ant-design/pro-form';
 
-import { message } from 'antd';
+import { message, Select, Spin } from 'antd';
+
+const { Option } = Select;
 
 interface CreateFormProps {
   trigger: JSX.Element;
@@ -22,15 +22,48 @@ const waitTime = (time: number = 100) => {
   });
 };
 
+
 const CreateForm: React.FC<CreateFormProps> = (props) => {
   // const { modalVisible, onCancel } = props;
+  const [fetching, setFetching] = useState<boolean>(false);
+  const [data, setData] = useState([]);
+
+  let lastFetchId = 0;
+
+  const fetchUser = async (value: string) => {
+    console.log('fetching user', value);
+    lastFetchId += 1;
+    const fetchId = lastFetchId;
+    setFetching(true);
+    setData([]);
+    fetch('https://randomuser.me/api/?results=5')
+      .then(response => response.json())
+      .then(body => {
+        if (fetchId !== lastFetchId) {
+          // for fetch callback order
+          return;
+        }
+        const res = body.results.map(user => ({
+          text: `${user.name.first} ${user.name.last}`,
+          value: user.login.username,
+        }));
+        setData(res);
+        setFetching(false);
+      });
+  };
+
+  const handleChange = value => {
+    console.log(value);
+    setData([]);
+    setFetching(false);
+  };
 
   return (
     <ModalForm<{
       name: string;
       company: string;
     }>
-      title="新建表单"
+      title='新建群组'
       trigger={
         props.trigger
       }
@@ -39,52 +72,38 @@ const CreateForm: React.FC<CreateFormProps> = (props) => {
       }}
       onFinish={async (values) => {
         await waitTime(2000);
-        console.log(values.name);
+        console.log(values);
         message.success('提交成功');
         return true;
       }}
     >
-      <ProForm.Group>
-        <ProFormText
-          width="md"
-          name="name"
-          label="签约客户名称"
-          tooltip="最长为 24 位"
-          placeholder="请输入名称"
-        />
-        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
-      </ProForm.Group>
-      <ProForm.Group>
-        <ProFormText width="md" name="contract" label="合同名称" placeholder="请输入名称" />
-        <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
-      </ProForm.Group>
-      <ProForm.Group>
-        <ProFormSelect
-          options={[
-            {
-              value: 'chapter',
-              label: '盖章后生效',
-            },
-          ]}
-          width="xs"
-          name="useMode"
-          label="合同约定生效方式"
-        />
-        <ProFormSelect
-          width="xs"
-          options={[
-            {
-              value: 'time',
-              label: '履行完终止',
-            },
-          ]}
-          name="unusedMode"
-          label="合同约定失效效方式"
-        />
-      </ProForm.Group>
-      <ProFormText width="sm" name="id" label="主合同编号" />
-      <ProFormText name="project" disabled label="项目名称" initialValue="xxxx项目" />
-      <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
+      <ProFormText
+        width='md'
+        name='identifier'
+        label='群组名称'
+        tooltip='最长为 255 位'
+        placeholder='请输入名称'
+      />
+      <ProForm.Item name='manager' label='负责人' className='pro-field-md'>
+        <Select
+          className='pro-field-md'
+          // mode='multiple'
+          showSearch
+          labelInValue
+
+          placeholder='Select users'
+          notFoundContent={fetching ? <Spin size='small' /> : null}
+          filterOption={false}
+          onSearch={fetchUser}
+          onChange={handleChange}
+          style={{ width: '100%' }}
+        >
+          {data.map(d => (
+            <Option key={d.value} value={'st'}>{d.text}</Option>
+          ))}
+        </Select>
+      </ProForm.Item>
+      <ProFormTextArea width='md' name='description' label='描述' />
     </ModalForm>
   );
 };
